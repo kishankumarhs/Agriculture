20b4e7f9993298eaf96f1d8d165b2cfb180e9c32
Kishan Kumar Hs <kishanKumar@users.noreply.github.com>
2022-01-25 20:43:30 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+//service section owl carousel
+$(".fruits_owl-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    autoHeight: true,
+    nav: true,
+    navText: [
+        '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        576: {
+            items: 2,
+        },
+        768: {
+            items: 3,
+        },
+        991: {
+            items: 4,
+        },
+    },
+});
